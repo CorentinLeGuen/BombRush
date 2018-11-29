d83df9fc5865455c4c98a7316be48b8429ba700e
Guillaume HELOUIS <1146421+ghelouis@users.noreply.github.com>
2018-11-29 14:44:08 +0100

    Add word checker

---
+++ src/main/java/com/bombrush/room/DicoController.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright © BNP PARIBAS - All rights reserved.
+ */
+package com.bombrush.room;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
+
+@Controller
+@RequestMapping("/dico")
+public class DicoController {
+
+    @Autowired
+    private
+    WordChecker wordChecker;
+
+    @GetMapping("/french")
+    public ResponseEntity checkIfFrenchWordExists(@RequestParam(name = "word") String word) {
+        return wordChecker.frenchWordExists(word) ? new ResponseEntity(OK) : new ResponseEntity(NOT_FOUND);
+    }
+}
